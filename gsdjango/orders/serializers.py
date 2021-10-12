4c105ef1b0ecd5a2471fa2bba6a5554bbf7a284f
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-10-13 00:56:20 +0530

    Create serializers.py

---
+++ gsdjango/orders/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import *
+from django.contrib.auth.models import User
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Orders
+        fields = ['user_id, 'product_code']
